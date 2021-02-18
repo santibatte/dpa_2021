5128be9a33f7139560a9d90eac11f6991d864f4d
Roberto919 <robertosysm@gmail.com>
2021-02-18 11:25:36 -0600

    pipeline ready to download data and delete temporal file

--- src/pipeline/ingesta_almacenamiento.py
+++ src/pipeline/ingesta_almacenamiento.py
@@ -13,10 +13,20 @@
 
 from datetime import date
 
+import sys
+
+import yaml
+
+import os
+
 
 ## Third party imports
 
 from sodapy import Socrata
+
+import boto3
+
+import pandas as pd
 
 
 ## Local application imports
@@ -30,16 +40,64 @@ from sodapy import Socrata
 ## Parameters ##
 ################
 
+
+## AWS parameters
+bucket_name = "data-product-architecture-equipo-super"
+
+hist_ingest_path = "ingestion/initial/"
+hist_dat_prefix = "historic-inspections-"
+
+cont_ingest_path = "ingestion/consecutive/"
+cont_dat_prefix = "consecutive-inspections-"
+
+
+## Token for API
 token = "5HfsId12lhMMzSlYANoAq451w"
+
+
+## Naming files
+today_info = date.today().strftime('%Y-%m-%d')
 
 
 
 
 
 "------------------------------------------------------------------------------"
-###############
-## Functions ##
-###############
+#########################
+## Ancillary functions ##
+#########################
+
+
+##
+def read_yaml_file(yaml_file):
+    """ load yaml cofigurations """
+
+    config = None
+    try:
+        with open (yaml_file, 'r') as f:
+            config = yaml.safe_load(f)
+    except:
+        raise FileNotFoundError('Couldnt load the file')
+
+    return config
+
+
+
+##
+def get_s3_credentials(credentials_file):
+    credentials = read_yaml_file(credentials_file)
+    s3_creds = credentials['s3']
+
+    return s3_creds
+
+
+
+
+
+"------------------------------------------------------------------------------"
+####################
+## Main functions ##
+####################
 
 
 ##
@@ -56,21 +114,76 @@ def ingesta_inicial(client, limit=300000):
 
 ##
 def ingesta_consecutiva(client, limit=1000):
-	today = date.today()
-	today = today.strftime('%Y-%m-%d')
 	return client.get("4ijn-s7e5", limit=limit)
 
 
 
-##
+## Getting an s3 resource to interact with AWS s3 based on .yaml file
 def get_s3_resource():
-	pass
+	"""
+	Getting an s3 resource to interact with AWS s3 based on .yaml file
+		args:
+			-
+		returns:
+			s3 (aws client session): s3 resource
+	"""
+
+	s3_creds = get_s3_credentials("../conf/local/credentials.yaml")
+
+	session = boto3.Session(
+	    aws_access_key_id=s3_creds['aws_access_key_id'],
+	    aws_secret_access_key=s3_creds['aws_secret_access_key']
+	)
+
+	s3 = session.client('s3')
+
+	return s3
 
 
 
-##
-def guardar_ingesta(name):
-	pass
+## Saving data donwloaded with Chicago's API
+def guardar_ingesta(bucket_name, bucket_path):
+	"""
+	Saving data donwloaded with Chicago's API
+		args:
+			- bucket_name (string): name of bucket where data will be stored.
+			- bucket_path (string): path within the bucket to store data.
+			- pkl_path (string): string with location of temporal pkl stored in local machine.
+	"""
+
+	## Getting s3 resource to store data in s3.
+	s3 = get_s3_resource()
+
+
+	## Getting client to download data with API
+	client = get_client(token)
+
+
+	## Downloading data and storing it temporaly in local machine prior upload to s3
+	if "initial" in bucket_path:
+		pkl_temp_local_path = "../data/" + hist_ingest_path + hist_dat_prefix + today_info + ".pkl"
+		pd.DataFrame(ingesta_inicial(client)).to_pickle(pkl_temp_local_path)
+
+	elif "consecutive" in bucket_path:
+		pkl_temp_local_path = "../data/" + cont_ingest_path + cont_dat_prefix + today_info + ".pkl"
+		pd.DataFrame(ingesta_consecutiva(client)).to_pickle(pkl_temp_local_path)
+
+	else:
+		raise NameError('Unknown bucket path')
+
+
+	## Uploading data to s3
+	file_name = pkl_temp_local_path.split(sep='/')[-1]
+	s3.upload_file(pkl_temp_local_path, bucket_name, bucket_path + file_name)
+
+
+	## Deleting temporal local pickle
+	os.remove(pkl_temp_local_path)
+
+
+	return
+
+
 
 
 
