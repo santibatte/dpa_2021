45b8660bc12a88f1c11f547066cf07e26c7a4ae4
Roberto919 <robertosysm@gmail.com>
2021-02-17 20:07:44 -0600

    updating file format

--- src/pipeline/ingesta_almacenamiento.py
+++ src/pipeline/ingesta_almacenamiento.py
@@ -1,26 +1,83 @@
 #!/usr/bin/env python
 
+
+
+
+
+"------------------------------------------------------------------------------"
+#############
+## Imports ##
+#############
+
+## Standard library imports
+
 from datetime import date
+
+
+## Third party imports
+
 from sodapy import Socrata
+
+
+## Local application imports
+
+
+
+
+
+"------------------------------------------------------------------------------"
+################
+## Parameters ##
+################
 
 token = "5HfsId12lhMMzSlYANoAq451w"
 
+
+
+
+
+"------------------------------------------------------------------------------"
+###############
+## Functions ##
+###############
+
+
+##
 def get_client(token):
 	return Socrata("data.cityofchicago.org", token)
 
 
+
+##
 def ingesta_inicial(client, limit=300000):
 	return client.get("4ijn-s7e5", limit=limit)
 
 
+
+##
 def ingesta_consecutiva(client, limit=1000):
 	today = date.today()
 	today = today.strftime('%Y-%m-%d')
 	return client.get("4ijn-s7e5", limit=limit)
 
 
+
+##
 def get_s3_resource():
 	pass
 
+
+
+##
 def guardar_ingesta(name):
 	pass
+
+
+
+
+
+"------------------------------------------------------------------------------"
+#################
+## END OF FILE ##
+#################
+"------------------------------------------------------------------------------"
