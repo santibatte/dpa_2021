4d23e1b9bb68112a1352e1c8d403680f7ad4b914
Roberto919 <robertosysm@gmail.com>
2021-03-10 13:20:19 -0600

    moving cleaning functions to ingestion module

--- src/etl/ingesta_almacenamiento.py
+++ src/etl/ingesta_almacenamiento.py
@@ -34,8 +34,6 @@ import pandas as pd
 
 ## Local application imports
 
-# sys.path.append("../")
-
 from src.utils.general import (
 	read_yaml_file,
 	get_s3_credentials,
@@ -69,9 +67,9 @@ today_info = date.today().strftime('%Y-%m-%d')
 
 
 "------------------------------------------------------------------------------"
-###############
-## Functions ##
-###############
+#######################################
+## Data loading and saving functions ##
+#######################################
 
 
 ##
@@ -171,6 +169,173 @@ def guardar_ingesta(bucket_name, bucket_path):
 
 	## Uploading data to s3
 	return s3.put_object(Bucket=bucket_name, Key=bucket_path + file_name, Body=ingesta)
+
+
+
+## (test function) Converting data into pandas dataframe by providing the data's location.
+def ingest_local_csv(data_path):
+    """
+    (test function) Converting data into pandas dataframe by providing the data's location.
+        args:
+            filename (string): path relative to the repo's root where the data (.csv file) is located
+        returns:
+            df_c5 (dataframe): .csv file converted to pandas dataframe
+    """
+
+    ## Reading file in specified path
+    df = pd.read_csv(data_path)
+
+    return df
+
+
+
+
+
+"------------------------------------------------------------------------------"
+#############################
+## Data cleaning functions ##
+#############################
+
+
+## Transform columns' names to standard format
+def clean_col_names(dataframe):
+    """
+    Transform columns' names to standard format (lowercase, no spaces, no points)
+        args:
+            dataframe (dataframe): df whose columns will be formatted.
+        returns:
+            dataframe (dataframe): df with columns cleaned.
+    """
+
+    ## Definition of cleaning funcitons that will be applied to the columns' names
+    fun1 = lambda x: x.lower() ## convert to lowercase
+    fun2 = lambda x: re.sub("( |¡|!|¿|\?|\.|,|;|:)", "_", x) ## eliminate spaces and punctuation signs for underscore
+    fun3 = lambda x: unicodedata.normalize("NFD", x).encode("ascii", "ignore").decode("utf-8") ## substitute accents for normal letters
+    funcs = [fun1, fun2, fun3]
+
+    ## Applying the defined functions to the columns' names
+    for fun in funcs:
+        dataframe.columns = [fun(col) for col in dataframe.columns]
+
+    return dataframe
+
+
+
+## Converting observatios for selected columns into lowercase
+def convert_lower(data, vars_lower):
+    """
+     Converting observatios for selected columns into lowercase
+        args:
+            data (dataframe): data that is being analyzed.
+            vars_lower (list): list of the columns' names in the dataframe that will be changed to lowercase.
+        returns:
+            data(dataframe): dataframe that is being analyzed with the observations (of the selected columns) in lowercase.
+    """
+    for x in vars_lower:
+        data[x]=data[x].str.lower()
+    return data
+
+
+
+## Cleaning columns with text
+def clean_txt(txt):
+    """
+    """
+
+    ## Setting everything to lowercase and substituting special characters with "-"
+    txt = re.sub('[^a-zA-Z0-9 \n\.]', '-', txt.lower())
+
+    ## Changing spaces with "_"clean_col_names
+    txt = re.sub(" ", "_", txt)
+
+
+    return txt
+
+
+
+## Identify if any serious violations were committed if
+def mark_serious_violations(row):
+    """
+    Identify if any serious violations were committed if
+
+    :param row: dataframe row where violations codes to be evaluated are present.
+
+    :return:
+    """
+    try:
+
+        v_nums = re.findall(r'\| (.+?). ', row)
+
+        if len(set(serious_viols) - set(v_nums)) == len(set(serious_viols)):
+            res = "no_serious_violations"
+
+        else:
+            res = "serious_violations"
+
+    except:
+        res = "no_result"
+
+    return res
+
+
+
+## Initial function to clean the dataset
+def initial_cleaning(data):
+    """
+    Initial function to clean the dataset
+
+    :param data: raw dataframe that will be go through the initial cleaning process
+
+    :return dfx: resulting dataframe after initial cleaning
+    """
+
+    ## Creating copy of initial dataframe
+    dfx = data.copy()
+
+
+    ## Cleaning names of columns
+    clean_col_names(dfx)
+
+
+    ## Creating column identifying if entry has serious violations
+
+    #### Adding specific string to beggining of `violations`
+    dfx["violations"] = "| " + dfx["violations"]
+
+    #### Creating new column with label regarding presence of serious violations
+    dfx["serious_violations"] = dfx["violations"].apply(lambda x: mark_serious_violations(x))
+
+
+    return dfx
+
+
+
+
+
+"------------------------------------------------------------------------------"
+###############################
+## Ingestion master function ##
+###############################
+
+
+## Function desigend to execute all ingestion functions.
+def ingest(data_path, ingestion_pickle_loc):
+    """
+    Function desigend to execute all ingestion functions.
+        args:
+            path (string): path where the project's data is stored.
+            ingestion_save (string): location where the resulting pickle object will be stored.
+        returns:
+            -
+    """
+
+    ## Executing ingestion functions
+    df = ingest_file(data_path)
+    df = set_index(df)
+    df = drop_cols(df)
+    df = generate_label(df)
+    save_ingestion(df, ingestion_pickle_loc)
+    print("\n** Ingestion module successfully executed **\n")
 
 
 
