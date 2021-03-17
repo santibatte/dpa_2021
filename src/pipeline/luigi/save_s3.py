c36fd8beb8c1e4db240fbc56e4e885aaebd89bf0
carlosgeovany <ccggppvv@hotmail.com>
2021-03-17 11:36:11 -0600

    save_s3.py

--- src/pipeline/luigi/save_s3.py
+++ src/pipeline/luigi/save_s3.py
@@ -34,7 +34,7 @@ class S3Task(luigi.Task):
         ingesta=pickle.load(open('src/pipeline/luigi/luigi_tmp_files/ingesta_tmp.pkl', 'rb')) ## cambiar nombre buscar el archivo initiarl.
         ingesta = pickle.dumps(ingesta)
 
-        #elif ingest_type.self == 'consecutive':
+        #elif ingest_type.self == 'consecutve':
             #ingesta=pickle.load(open('src/pipeline/luigi/luigi_tmp_files/ingesta_tmp.pkl', 'rb')) ## cambiar nombre, mirar el archivo llamado CONSECUTIVE.
             #ingesta = pickle.dumps(ingesta)
 
