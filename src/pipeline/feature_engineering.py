51a68076b4e7d89e2aef21fb75c8ad0dfe8de136
Roberto919 <robertosysm@gmail.com>
2021-04-18 22:46:22 -0500

    importing metadata function

--- src/pipeline/feature_engineering.py
+++ src/pipeline/feature_engineering.py
@@ -43,7 +43,8 @@ from src.utils.data_dict import (
 from src.utils.utils import (
     json_dump_dict,
     load_df,
-    save_df
+    save_df,
+    write_csv_from_df
 )
 
 from src.utils.params_gen import (
