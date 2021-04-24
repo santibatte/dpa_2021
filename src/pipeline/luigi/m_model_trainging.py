4d5b99c90f7b7e38d576d74bc318d0c82e8ff769
Roberto919 <robertosysm@gmail.com>
2021-04-24 10:01:18 -0500

    upating luigi module to obtain model training results

--- src/pipeline/luigi/m_model_trainging.py
+++ src/pipeline/luigi/m_model_trainging.py
@@ -7,27 +7,39 @@ import pickle
 
 from src.pipeline.luigi.l_feature_engineering_metadata import FeatureEngineeringMetadata
 
+from src.utils.params_gen import (
+    mt_results_pickle_loc,
+    today_info,
+)
+
+
+
+
+
 class ModelTraining(luigi.Task):
 
+
+    ## Requires: assessing that feature engineering metadata is stored
     def requires(self):
         return FeatureEngineeringMetadata(ingest_type=self.ingest_type, bucket=self.bucket)
 
 
+    ## Run: training models with engineered features
     def run(self):
 
         s3 = get_s3_resource()
 
-        feature_engineering_pickle_loc_s3 = 'feature_engineering/feature_engineering_' + today_info + '.pkl'
+        fe_pickle_loc_s3 = 'feature_engineering/feature_engineering_' + today_info + '.pkl'
 
-        model_training_luigi = s3.get_object(Bucket=self.bucket, Key=feature_engineering_luigi)
+        fe_results_pkl = s3.get_object(Bucket=self.bucket, Key=fe_pickle_loc_s3)
 
-        df_pre_training = pickle.loads(model_training_luigi['Body'].read())
+        fe_results_dict = pickle.loads(fe_results_pkl['Body'].read())
 
-        model_training = aqui_entrenamos_modelo  con parametro : df_pre_transformation
+        mt_results_dict = models_training(fe_results_dict, mt_results_pickle_loc)
 
-        model_training_pkl = pickle.dumps(model_training)
+        mt_pkl = pickle.dumps(mt_results_dict)
 
-        s3.put_object(Bucket=self.bucket, Key=get_key(self.output().path), Body=model_training_pkl)
+        s3.put_object(Bucket=self.bucket, Key=get_key(self.output().path), Body=mt_pkl)
 
 
     ## Output: uploading data to s3 path
@@ -42,6 +54,6 @@ class ModelTraining(luigi.Task):
             'trained_models',
         )
 
-        output_path = output_path_start + 'trained_models_' +  today_info +'.pkl'
+        output_path = output_path_start + 'trained_models_' + today_info + '.pkl'
 
         return luigi.contrib.s3.S3Target(output_path, client=client)
