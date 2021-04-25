c6d35358f3ac944c522611e6d2e230e94ef411cc
santibatte <santiagobattezzati@gmail.com>
2021-04-24 21:27:39 -0500

    corrige imports

--- src/pipeline/luigi/m_model_training.py
+++ src/pipeline/luigi/m_model_training.py
@@ -5,6 +5,13 @@ import luigi.contrib.s3
 import pickle
 
 
+from src.utils.utils import (
+    get_s3_resource,
+    get_s3_resource_luigi,
+    get_key,
+    write_csv_from_df,
+)
+
 from src.pipeline.luigi.l_feature_engineering_metadata import FeatureEngineeringMetadata
 
 class ModelTraining(luigi.Task):
