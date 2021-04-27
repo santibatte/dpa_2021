8927ee4c20640f6653ccb968a16b16e9b635823d
santibatte <santiagobattezzati@gmail.com>
2021-04-27 10:37:25 -0500

    corrige indentacion

--- src/pipeline/models_training.py
+++ src/pipeline/models_training.py
@@ -102,12 +102,12 @@ def magic_loop(models_dict, fe_results_dict):
 
     res = []
     with open(tests_dir_loc + "train_unittest.txt") as fp:
-    lines = fp.readlines()
-    for line in lines:
-        if "FAILED" in line:
-            res.append([str(datetime.now()), "FAILED, Your X_train have less than 50 rows"])
-        if "OK" in line:
-            res.append([str(datetime.now()), "PASS"])
+        lines = fp.readlines()
+        for line in lines:
+            if "FAILED" in line:
+                res.append([str(datetime.now()), "FAILED, Your X_train have less than 50 rows"])
+            if "OK" in line:
+                res.append([str(datetime.now()), "PASS"])
 
     res_df = pd.DataFrame(res, columns=['Date', 'Result'])
 
