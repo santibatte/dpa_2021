e41106c6d8233ac25688bc87e3d03aef77584a55
Roberto919 <robertosysm@gmail.com>
2021-05-04 14:15:17 -0500

    creating missing files in luigi dir

---
+++ src/pipeline/luigi/o_model_training_metadata.py
@@ -0,0 +1,52 @@
+from luigi.contrib.postgres import CopyToTable
+
+import pandas as pd
+import luigi
+import psycopg2
+
+
+from src.utils.utils import (
+    get_postgres_credentials
+)
+
+from src.pipeline.luigi.n_model_training_test import ModelTrainingTest
+
+csv_local_file = "src/pipeline/luigi/luigi_tmp_files/models_training_metadata.csv"
+
+
+
+class ModelTrainingMetadata(CopyToTable):
+
+    #### Bucket where all ingestions will be stored in AWS S3
+    bucket = luigi.Parameter()
+
+    #### Defining the ingestion type to Luigi (`consecutive` or `initial`)
+    ingest_type = luigi.Parameter()
+
+    csv_local_file = "src/pipeline/luigi/luigi_tmp_files/models_training_metadata.csv"
+
+    def requires(self):
+        return ModelTrainingTest(ingest_type=self.ingest_type, bucket=self.bucket)
+
+    credentials = get_postgres_credentials("conf/local/credentials.yaml")
+
+    user = credentials['user']
+    password = credentials['pass']
+    database = credentials['db']
+    host = credentials['host']
+    port = credentials['port']
+    table = 'dpa_metadata.model_training'
+
+
+## ADAPTAR al numero de columnas correctas
+    columns = [("execution_time", "VARCHAR"),
+               ("no_models_trained", "VARCHAR"),
+               ("type_models_trained", "VARCHAR")]
+
+
+
+    def rows(self):
+        reader = pd.read_csv(csv_local_file, header=None)
+
+        for element in reader.itertuples(index=False):
+            yield element
