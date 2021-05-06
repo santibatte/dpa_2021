f896448b9c0a886a1167e5ccf280c9549d3c3191
Roberto919 <robertosysm@gmail.com>
2021-05-05 19:32:54 -0500

    adding zip classification function to transformation module and emptying create_referece_group module

--- src/etl/transformation.py
+++ src/etl/transformation.py
@@ -60,6 +60,10 @@ from src.utils.params_gen import (
 
     regex_violations,
     serious_viols,
+
+    high_income_zip_codes,
+    medium_income_zip_codes,
+    low_income_zip_codes,
 )
 
 
@@ -287,6 +291,34 @@ def serious_viols_col(df):
     ## Updating transformation counts
     global trans_count
     trans_count += 1
+
+
+    return df
+
+
+
+## Creating new column zip codes classified by income level
+def create_reference_group(df):
+    """
+    df a df with zip code
+    returns a df with new column zip_income_classification which can be used as a reference group in aequitas analysis
+    """
+
+    zip_food_list=list(df['Zip'])
+
+    classification_food = []
+
+    for val in zip_food_list:
+        if val in high_income_zip_codes:
+            classification_food.append('High')
+        elif val in medium_income_zip_codes:
+            classification_food.append('Medium')
+        elif val in low_income_zip_codes:
+            classification_food.append('Low')
+        else:
+            classification_food.append('Other')
+
+    df['Zip_income_classification'] = classification_food
 
 
     return df
--- src/pipeline/create_reference_group.py
+++ src/pipeline/create_reference_group.py
@@ -1,25 +1,3 @@
 
 
-def create_reference_group(df):
-    """
-    df a df with zip code
-    returns a df with new column zip_income_classification which can be used as a reference group in aequitas analysis
-    """
 
-    zip_food_list=list(df['Zip'])
-
-    classification_food = []
-
-    for val in zip_food_list:
-        if val in high_income_zip_codes:
-            classification_food.append('High')
-        elif val in medium_income_zip_codes:
-            classification_food.append('Medium')
-        elif val in low_income_zip_codes:
-            classification_food.append('Low')
-        else:
-            classification_food.append('Other')
-
-    df['Zip_income_classification'] = classification_food
-
-    return df
