363ba7dfddd57d45d4214ea4b5a1aa0679a75d94
Roberto919 <robertosysm@gmail.com>
2021-05-21 11:11:45 -0500

    creating .sh file to execute luigi more easily

---
+++ dpa_main_luigi_exe.sh
@@ -0,0 +1,2 @@
+## Final Task: BiasFairnessMetadata
+luigi --module src.pipeline.luigi.u_bias_fairness_metadata BiasFairnessMetadata --ingest-type consecutive --bucket data-product-architecture-equipo-9
