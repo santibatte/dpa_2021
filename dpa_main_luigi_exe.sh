b632df3d3965c11bc1a73348e329f9e5423b1972
Roberto919 <robertosysm@gmail.com>
2021-05-21 11:20:37 -0500

    adding distinction between remote and local scheduler in .sh

--- dpa_main_luigi_exe.sh
+++ dpa_main_luigi_exe.sh
@@ -1,2 +1,5 @@
 ## Final Task: BiasFairnessMetadata
-luigi --module src.pipeline.luigi.u_bias_fairness_metadata BiasFairnessMetadata --ingest-type consecutive --bucket data-product-architecture-equipo-9
+#### Local Scheduler
+luigi --module src.pipeline.luigi.u_bias_fairness_metadata BiasFairnessMetadata --ingest-type consecutive --bucket data-product-architecture-equipo-9 --local-scheduler
+#### Remote Scheduler
+#luigi --module src.pipeline.luigi.u_bias_fairness_metadata BiasFairnessMetadata --ingest-type consecutive --bucket data-product-architecture-equipo-9
