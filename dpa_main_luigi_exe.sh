438711694ac4a8ae2d2d8e382ee46d409c5b0fea
Roberto919 <robertosysm@gmail.com>
2021-05-21 11:22:07 -0500

    minor updates in .sh

--- dpa_main_luigi_exe.sh
+++ dpa_main_luigi_exe.sh
@@ -3,3 +3,5 @@
 luigi --module src.pipeline.luigi.u_bias_fairness_metadata BiasFairnessMetadata --ingest-type consecutive --bucket data-product-architecture-equipo-9 --local-scheduler
 #### Remote Scheduler
 #luigi --module src.pipeline.luigi.u_bias_fairness_metadata BiasFairnessMetadata --ingest-type consecutive --bucket data-product-architecture-equipo-9
+
+##
