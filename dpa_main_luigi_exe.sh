1e42af23569ca49a5721c821f9acdd5b1ebc7772
Roberto919 <robertosysm@gmail.com>
2021-05-21 11:29:51 -0500

    adding PredictMetadata task to .sh

--- dpa_main_luigi_exe.sh
+++ dpa_main_luigi_exe.sh
@@ -1,7 +1,19 @@
+## SHELL SCRIPT TO EXECUTE LUIGI TASKS
+
+
+
+
+
 ## Final Task: BiasFairnessMetadata
 #### Local Scheduler
-luigi --module src.pipeline.luigi.u_bias_fairness_metadata BiasFairnessMetadata --ingest-type consecutive --bucket data-product-architecture-equipo-9 --local-scheduler
+#luigi --module src.pipeline.luigi.u_bias_fairness_metadata BiasFairnessMetadata --ingest-type consecutive --bucket data-product-architecture-equipo-9 --local-scheduler
 #### Remote Scheduler
 #luigi --module src.pipeline.luigi.u_bias_fairness_metadata BiasFairnessMetadata --ingest-type consecutive --bucket data-product-architecture-equipo-9
 
-##
+
+
+## Final Task: PredictMetadata
+#### Local Scheduler
+luigi --module src.pipeline.luigi.x_predict_metadata PredictMetadata --ingest-type consecutive --bucket data-product-architecture-equipo-9 --local-scheduler
+#### Remote Scheduler
+#luigi --module src.pipeline.luigi.x_predict_metadata PredictMetadata --ingest-type consecutive --bucket data-product-architecture-equipo-9
