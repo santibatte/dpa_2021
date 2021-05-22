e05145ee8d7ed6efe89ee6661c0547971c0b2274
root <root@ip-10-0-0-56.us-west-2.compute.internal>
2021-05-22 16:51:16 +0000

    soluciona lo del header en la tabla

--- src/pipeline/luigi/z_monitor.py
+++ src/pipeline/luigi/z_monitor.py
@@ -53,7 +53,7 @@ class Monitor(CopyToTable):
 
 
     def rows(self):
-        reader = pd.read_csv(csv_local_file, header=True)
+        reader = pd.read_csv(csv_local_file, header=0)
 
         for element in reader.itertuples(index=False):
             yield element
