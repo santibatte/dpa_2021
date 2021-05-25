7dfd9cbf6fcc5ffb5f1c08bed0a7f144efa2ecdd
santibatte <santiagobattezzati@gmail.com>
2021-05-25 15:05:26 -0500

    updates sql

--- sql/create_schema_and_tables.sql
+++ sql/create_schema_and_tables.sql
@@ -1,9 +1,11 @@
 #sql create schema and tables 
 
-DROP SCHEMA IF EXISTS dpa_metadata, dpa_unittest CASCADE;
+DROP SCHEMA IF EXISTS dpa_metadata, dpa_unittest, dpa_storeapi, dpa_monitor CASCADE;
 
 CREATE SCHEMA dpa_metadata;
 CREATE SCHEMA dpa_unittest;
+CREATE SCHEMA dpa_storeapi;
+CREATE SCHEMA dpa_monitor;
 
 CREATE TABLE dpa_metadata.extract (
 	extraction_time varchar DEFAULT NULL,
@@ -35,4 +37,62 @@ CREATE TABLE dpa_metadata.feature_engineering (
 	name_num_features varchar DEFAULT NULL,
 	shape_after_fe varchar DEFAULT NULL
 );
+
+CREATE TABLE dpa_metadata.model_training (
+	execution_time varchar DEFAULT NULL,
+	no_models_trained varchar DEFAULT NULL,
+	type_models_trained varchar DEFAULT NULL
+);
+
+CREATE TABLE dpa_metadata.model_selection (
+	execution_time varchar DEFAULT NULL,
+	training_score varchar DEFAULT NULL,
+	selected_model varchar DEFAULT NULL
+);
+
+CREATE TABLE dpa_metadata.bias_fairness (
+	execution_time varchar DEFAULT NULL,
+	value_k varchar DEFAULT NULL,
+	FOR_p varchar DEFAULT NULL,
+	FNR_p varchar DEFAULT NULL
+
+);
+
+CREATE TABLE dpa_metadata.predictions (
+	execution_time varchar DEFAULT NULL,
+	predict_model varchar DEFAULT NULL,
+	percentage_positives varchar DEFAULT NULL,
+	mean_score_positives varchar DEFAULT NULL
+
+);
+
+
+ 
+CREATE TABLE dpa_storeapi.store_predictions (
+	id_client varchar DEFAULT NULL,
+	prediction_date varchar DEFAULT NULL,
+	model_label varchar DEFAULT NULL,
+	score_label_0 varchar DEFAULT NULL,
+	score_label_1 varchar DEFAULT NULL
+
+
+);
+ 
+
+
+CREATE TABLE dpa_monitor.monitor (
+	id_client varchar DEFAULT NULL,
+	prediction_date varchar DEFAULT NULL,
+	model_label varchar DEFAULT NULL,
+	score_label_0 varchar DEFAULT NULL,
+	score_label_1 varchar DEFAULT NULL
+
+
+);
+ 
+ 
+
+ 
+
+
 
